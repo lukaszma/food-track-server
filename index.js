8ce9691589311b98d7c9f89a1f588bc77498ac9b
Łukasz Matyja <lukasz.matyja@x-insight.pl>
2024-03-04 14:16:27 +0100

    init

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import webpush from "web-push";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+const vapidKeys = {
+  publicKey: process.env.VAPID_PUBLIC_KEY,
+  privateKey: process.env.VAPID_PRIVATE_KEY,
+};
+
+webpush.setVapidDetails(
+  "mailto:infex@o2.pl",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+let subscriptions = [];
+
+app.post("/subscribe", (req, res) => {
+    console.log(req.body)
+  const subscription = req.body;
+  subscriptions.push(subscription);
+
+  res.status(201).json({ status: "success" });
+});
+
+app.post("/send-notification", (req, res) => {
+  const notificationPayload = {
+    title: "New Notification",
+    body: "This is a new notification",
+    // icon: "https://some-image-url.jpg",
+    data: {
+      url: "https://example.com",
+    },
+  };
+
+
+  Promise.all(
+    subscriptions.map((subscription) =>
+      webpush.sendNotification(
+        subscription,
+        JSON.stringify(notificationPayload)
+      )
+    )
+  )
+    .then(() =>
+      res.status(200).json({ message: "Notification sent successfully." })
+    )
+    .catch((err) => {
+      console.log(err);
+      console.error("Error sending notification");
+      res.sendStatus(500);
+    });
+});
+
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
+});
